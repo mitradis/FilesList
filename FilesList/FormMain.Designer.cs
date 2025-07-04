7f757242055efe3679bebfd1fc16889939816718
Vitaliy <mitradis@gmail.com>
2025-07-04 14:07:56 +0300

    Upload

---
+++ FilesList/FormMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FilesList
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(7, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Путь";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(114, 7);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Атрибуты файла";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(250, 7);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(75, 20);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "CRC32";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(326, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Архив";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox4.Location = new System.Drawing.Point(250, 25);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(75, 20);
+            this.checkBox4.TabIndex = 2;
+            this.checkBox4.Text = "Папки";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.Location = new System.Drawing.Point(114, 25);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(109, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Read Only";
+            // 
+            // FormMain
+            // 
+            this.ClientSize = new System.Drawing.Size(394, 51);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Files List";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ FilesList/FormMain.cs
@@ -0,0 +1,382 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilesList
+{
+    public partial class FormMain : Form
+    {
+        List<string> outList = new List<string>();
+        string lastPath = null;
+        string reader = null;
+        int pathLength = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            bool found = false;
+            RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            if (baseKey != null)
+            {
+                RegistryKey regkey = baseKey.OpenSubKey("SOFTWARE\\7-Zip");
+                if (regkey != null)
+                {
+                    string path = (string)regkey.GetValue("Path");
+                    if (path != null && File.Exists(Path.Combine(path, "7z.exe")))
+                    {
+                        reader = Path.Combine(path, "7z.exe");
+                        found = true;
+                    }
+                    regkey.Dispose();
+                }
+                baseKey.Dispose();
+            }
+            if (!found)
+            {
+                ClientSize = new System.Drawing.Size(318, 51);
+                button2.Visible = false;
+            }
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            enableDisable(false);
+            if (lastPath != null)
+            {
+                folderBrowserDialog1.SelectedPath = lastPath;
+            }
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                lastPath = folderBrowserDialog1.SelectedPath;
+                pathLength = pathAddSlash(lastPath).Length;
+                searchFolder(lastPath);
+                writeFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), checkFolder()));
+            }
+            enableDisable(true);
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            enableDisable(false);
+            if (lastPath != null)
+            {
+                openFileDialog1.InitialDirectory = lastPath;
+            }
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                lastPath = Path.GetDirectoryName(openFileDialog1.FileNames[0]);
+                bool multi = openFileDialog1.FileNames.Length > 1;
+                foreach (string line in openFileDialog1.FileNames)
+                {
+                    parseArchive(line, multi);
+                }
+            }
+            enableDisable(true);
+        }
+
+        void parseArchive(string file, bool multi)
+        {
+            string[] output = null;
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.StandardOutputEncoding = Encoding.GetEncoding(866);
+                process.StartInfo.FileName = reader;
+                process.StartInfo.Arguments = "l -slt \"" + file + "\"";
+                process.Start();
+                output = process.StandardOutput.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                process.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось запустить: " + reader + " va \"" + file + "\"");
+            }
+            if (output != null)
+            {
+                string path = null;
+                string size = null;
+                string modified = null;
+                string crc = null;
+                bool folder = false;
+                bool start = false;
+                bool end = false;
+                int count = output.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    if (!start && output[i].StartsWith("----------"))
+                    {
+                        start = true;
+                    }
+                    else if (start)
+                    {
+                        end = i + 1 == count;
+                        if (!String.IsNullOrEmpty(output[i]) || end)
+                        {
+                            if (end || output[i].StartsWith("Path = "))
+                            {
+                                if (!String.IsNullOrEmpty(path) && !String.IsNullOrEmpty(size) && !String.IsNullOrEmpty(modified) && !String.IsNullOrEmpty(crc))
+                                {
+                                    if (folder)
+                                    {
+                                        if (checkBox4.Checked)
+                                        {
+                                            outList.Add(path + (checkBox1.Checked ? "\t" + modified : ""));
+                                        }
+                                    }
+                                    else
+                                    {
+                                        outList.Add(path + (checkBox2.Checked ? "\t" + crc : "") + (checkBox1.Checked ? "\t" + size + "\t" + modified : ""));
+                                    }
+                                }
+                                path = null;
+                                size = null;
+                                modified = null;
+                                crc = null;
+                                folder = false;
+                                if (!end)
+                                {
+                                    path = output[i].Remove(0, 7);
+                                }
+                            }
+                            else if (output[i].StartsWith("Folder = "))
+                            {
+                                folder = output[i].Remove(0, 9) == "+";
+                            }
+                            else if (output[i].StartsWith("Size = "))
+                            {
+                                size = output[i].Remove(0, 7);
+                            }
+                            else if (output[i].StartsWith("Modified = "))
+                            {
+                                modified = output[i].Remove(0, 11);
+                                modified = modified.Remove(modified.IndexOf('.'));
+                                DateTime parse;
+                                if (DateTime.TryParse(modified, out parse))
+                                {
+                                    modified = parse.ToString();
+                                }
+                            }
+                            else if (output[i].StartsWith("CRC = "))
+                            {
+                                crc = output[i].Remove(0, 6);
+                            }
+                        }
+                    }
+                }
+                string dir = null;
+                if (multi)
+                {
+                    dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), checkFolder());
+                    if (!Directory.Exists(dir))
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Не удалось создрать папку: " + dir);
+                        }
+                    }
+                }
+                writeFile(multi ? Path.Combine(dir, Path.GetFileNameWithoutExtension(file)) : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), Path.GetFileNameWithoutExtension(file)));
+            }
+        }
+
+        string checkFolder()
+        {
+            DirectoryInfo info = new DirectoryInfo(lastPath);
+            return info.Parent != null ? info.Name : info.Name.Remove(1);
+        }
+
+        void searchFolder(string path)
+        {
+            getFilesList(path);
+            foreach (string line in getDirectories(path))
+            {
+                if (Directory.Exists(line) && (new DirectoryInfo(line).Attributes & FileAttributes.System) != FileAttributes.System)
+                {
+                    if (checkBox4.Checked)
+                    {
+                        try
+                        {
+                            outList.Add(line.Remove(0, pathLength) + (checkBox1.Checked ? "\t" + Directory.GetLastWriteTime(line) : ""));
+                        }
+                        catch
+                        {
+                            outList.Add(line.Remove(0, pathLength) + "\tNO ACCESS TO FOLDER");
+                        }
+                    }
+                    searchFolder(line);
+                }
+            }
+        }
+
+        void getFilesList(string path)
+        {
+            foreach (string line in getFiles(path))
+            {
+                if (!File.GetAttributes(line).HasFlag(FileAttributes.System))
+                {
+                    try
+                    {
+                        FileInfo info = new FileInfo(line);
+                        outList.Add(line.Remove(0, pathLength) + (checkBox2.Checked ? "\t" + getCRC(line) : "") + (checkBox1.Checked ? "\t" + info.Length + "\t" + info.LastWriteTime : "") + (checkBox3.Checked && info.IsReadOnly ? "\tREAD-ONLY" : ""));
+                    }
+                    catch
+                    {
+                        outList.Add(line.Remove(0, pathLength) + "\tNO ACCESS TO FILE");
+                    }
+                }
+            }
+        }
+
+        string[] getFiles(string path)
+        {
+            try
+            {
+                return Directory.GetFiles(path);
+            }
+            catch
+            {
+                return new string[] { };
+            }
+        }
+
+        string[] getDirectories(string path)
+        {
+            try
+            {
+                return Directory.GetDirectories(path);
+            }
+            catch
+            {
+                return new string[] { };
+            }
+        }
+
+        string getCRC(string file)
+        {
+            string line = "";
+            FileStream fs = File.OpenRead(file);
+            line = String.Format("{0:X}", calculateCRC(fs));
+            fs.Close();
+            while (line.Length < 8)
+            {
+                line = "0" + line;
+            }
+            return line;
+        }
+
+        uint calculateCRC(Stream stream)
+        {
+            const int buffer_size = 1024;
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint result = 0xFFFFFFFF;
+            uint Crc32;
+            byte[] buffer = new byte[buffer_size];
+            uint[] table = new uint[256];
+            unchecked
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    Crc32 = (uint)i;
+                    for (int j = 8; j > 0; j--)
+                    {
+                        if ((Crc32 & 1) == 1)
+                        {
+                            Crc32 = (Crc32 >> 1) ^ POLYNOMIAL;
+                        }
+                        else
+                        {
+                            Crc32 >>= 1;
+                        }
+                    }
+                    table[i] = Crc32;
+                }
+                int count = stream.Read(buffer, 0, buffer_size);
+                while (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        result = ((result) >> 8) ^ table[(buffer[i]) ^ ((result) & 0x000000FF)];
+                    }
+                    count = stream.Read(buffer, 0, buffer_size);
+                }
+            }
+            buffer = null;
+            table = null;
+            stream.Close();
+            return ~result;
+        }
+
+        void writeFile(string file)
+        {
+            if (File.Exists(file + ".txt"))
+            {
+                int i = 1;
+                while (true)
+                {
+                    if (File.Exists(file + " (" + i.ToString() + ")" + ".txt"))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        file = file + " (" + i.ToString() + ")" + ".txt";
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                file = file + ".txt";
+            }
+            try
+            {
+                outList.Sort();
+                File.WriteAllLines(file, outList);
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось записать файл: " + file);
+            }
+            outList.Clear();
+        }
+
+        void enableDisable(bool enable)
+        {
+            button1.Text = enable ? "Путь" : "Работает";
+            button1.Enabled = enable;
+            button2.Enabled = enable;
+            checkBox1.Enabled = enable;
+            checkBox2.Enabled = enable;
+            checkBox3.Enabled = enable;
+            checkBox4.Enabled = enable;
+        }
+
+        string pathAddSlash(string path)
+        {
+            if (!path.EndsWith("/") && !path.EndsWith(@"\"))
+            {
+                if (path.Contains("/"))
+                {
+                    path += "/";
+                }
+                else if (path.Contains(@"\"))
+                {
+                    path += @"\";
+                }
+            }
+            return path;
+        }
+    }
+}
